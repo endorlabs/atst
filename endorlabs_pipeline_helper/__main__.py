b135d205d158e8d2a08e0b456aace7b23018a65f
Darren P Meyer <darrenpmeyer@users.noreply.github.com>
2023-09-15 14:38:53 -0700

    Minimal framework -- setup and GitHub CI awareness (#1)
    * checkpoint 22:06:10
    
    * Move statuswriter
    
    * first pass at command-streaming
    
    * Github Workflow and CLI rename
    
    * Fix pip install args for workflow
    
    * Fix install URL (trying with full ref name)
    
    * GitHub workflow for testing: using `install .`
    
    pip install from git URL is annoying while the repo is private
    
    * Workflow: build venv first
    
    * Added ability to download endorctl with `setup` sub-command
    
    * enable workflow debug
    
    * Updaate arch_map and check HTTP status`
    
    * debugging writeable paths
    
    * alternate pip install
    
    * pip alternate rev 2
    
    * pip alternate rev 3
    
    * pip alternate rev 4
    
    * pip alternate rev 5
    
    * pip alternate ver 6
    
    * pip alternate rev 7
    
    * pip alternate rev 8
    
    * pip alternate ver 9
    
    * pip alternate rev 10
    
    * pip alternate rev 11
    
    * pip alternate rev 12
    
    * pip alternate rev 13
    
    * pip alternate reverted
    
    * workflow path fun
    
    * GitHub CI detection
    
    * Trust endorctl to be in path
    
    * Fix import path for detected_CI
    
    * Fix missing module
    
    * Fix CI object inheritance
    
    * improve GitHub CI integration
    
    * workflow changes to reflect auto path setting
    
    * fix file appending in CI environments
    
    * Improve OS/ARCH detection flow for CI env
    
    * setup - handle auth by persisting to environment
    
    * setup - fix typo in GITHUB_ENV var name
    
    * workflow - show env
    
    * add newlines for env var
    
    * fix typo
    
    * add namespace to workflow
    
    * debugging api creds
    
    * debugging of API key passthru
    
    * more passthru debugging
    
    * slim down workflow
    
    * clean workflow

---
+++ endorlabs_pipeline_helper/__main__.py
@@ -0,0 +1,6 @@
+import warnings
+
+from .main import *
+from .cmd_setup import setup
+
+warnings.filterwarnings('ignore')
---
+++ endorlabs_pipeline_helper/utils/osarch.py
@@ -0,0 +1,22 @@
+# stdlib
+import os
+
+OS_MAP = {
+    'darwin': 'macos',
+    'gnu/linux': 'linux',
+    'linux': 'linux'
+}
+
+ARCH_MAP = {
+    'x86_64': 'amd64',
+    'x64': 'amd64',
+    'arm64': 'arm64',
+    'amd64': 'amd64'
+}
+
+def get_osarch(osname=None, arch=None):
+    uname = os.uname()
+    _osname = OS_MAP.get(uname.sysname.lower(), uname.sysname.lower()) if osname is None else osname
+    _arch = ARCH_MAP.get(uname.machine.lower(), uname.machine.lower()) if arch is None else arch
+
+    return(_osname, _arch)
